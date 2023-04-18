b91affb621df414a346d5a206c65dc738414170b
Pancakeq <andrii.bolinovskyi@gmail.com>
2023-04-18 13:16:08 +0300

    Url-validation-added

--- InforceTask/Controllers/LinksController.cs
+++ InforceTask/Controllers/LinksController.cs
@@ -39,7 +39,11 @@ namespace InforceTask.Controllers
                 Model.ErrorMessage = "Such Url exists";
                 return View(Model);
             }
-
+            if (!model.LongLink.Contains("https://"))
+            {
+                Model.ErrorMessage = "Url is not valid";
+                return View(Model);
+            }
             var id = Guid.NewGuid();
             var link = new Link
             {
